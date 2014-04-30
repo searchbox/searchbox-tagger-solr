82b36510784aab44d0aa250cb4268b767271fe04
Jonathan Rey <jonatrey@reziew.com>
2014-04-30 16:32:18 +0200

    Cleaning up the pom.xml

--- doall.sh
+++
@@ -1,3 +0,0 @@
-mvn -T compile package -DskipTests=true
-cp ./target/searchbox-tagger-1.0-SNAPSHOT-jar-with-dependencies.jar /salsasvn/projects/solr-4.1.0/example/solr/solrLib
-cp ./target/searchbox-tagger-1.0-SNAPSHOT-jar-with-dependencies.jar /salsasvn/searchbox-server/solr/solrLib
